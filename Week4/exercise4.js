ecd30b8906adb6eaf057b694d889903027b6fb1b
nikhil <nikhiljoseph36@gmail.com>
2022-02-28 17:05:59 +0000

    week 4 exercise4 chnages

--- Week4/exercise4.js
+++ Week4/exercise4.js
@@ -2,6 +2,8 @@ function changeHeading(){
 
     var element = document.getElementById("programHead");
     element.firstChild.nodeValue = "My Programming";
+    var paragrah = document.getElementById("para");
+    element.firstChild.nodeValue = "Hello to Computer Programming";
 
 }
 document.addEventListener('DOMContentLoaded',changeHeading);
